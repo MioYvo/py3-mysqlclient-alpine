c36ad82f235a522d439228617f9e8453b3aa53b5
Mio Lau <liurusi.101@gmail.com>
2017-06-26 17:38:43 +0800

    [add] main configurator

---
+++ configurator.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# __author__ = 'Mio'
+
+import requests
+
+configurator_host = "configurator"
+configurator_port = "8080"
+"""
+import configurator
+
+configurator.get_config()
+"""
+
+def get_config(id):
+
+    requests.get(configurator_host, )
---
+++ main.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# __author__ = 'Mio'
+
+import configurator
+
+configurator.get_config()
